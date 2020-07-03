aafac3618856e5accfe395d5db308f6ea6cc5edd
James Grieve <jgrieve@ualberta.ca>
2020-07-03 14:21:56 -0600

    Added constants for the elements we'll be interacting with;

---
+++ Image Gallery/js/scripts.js
@@ -0,0 +1,8 @@
+// <main> (the parent container of the images)
+const imageGallery = document.querySelector("main");
+// List of all images in main
+const images = imageGallery.querySelectorAll("img");
+// Search bar in the header
+const searchBar = document.querySelector("#search");
+// Clear button for the search bar
+const clearButton = document.querySelector("#clear");
