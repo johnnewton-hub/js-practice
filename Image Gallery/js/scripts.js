bec4d945111de2494269ce4707f412009f9ec864
James Grieve <jgrieve@ualberta.ca>
2020-07-03 15:07:46 -0600

    Added borders and captions to images;

--- Image Gallery/js/scripts.js
+++ Image Gallery/js/scripts.js
@@ -6,3 +6,45 @@ const images = imageGallery.querySelectorAll("img");
 const searchBar = document.querySelector("#search");
 // Clear button for the search bar
 const clearButton = document.querySelector("#clear");
+
+for (image of images)
+{
+    // Create a new element;
+    let newDiv = document.createElement("div");
+
+    // 1. Find the image's parent
+    // 2. Replace the image as a child of the parent with the div
+    // 3. Add the image as a child to the div
+
+    // Main > Img 
+    // Becomes
+    // Main > Div > Img
+
+    // 1.
+    let parentOfTestImage = image.parentNode;
+    // Nothing changes yet.
+
+    // 2.
+    parentOfTestImage.replaceChild(newDiv, image);
+    // Main > Div (no Img, got replaced with Div)
+
+    // 3.
+    newDiv.appendChild(image);
+    // Main > Div > Img
+
+    // Generate a paragraph to store the caption.
+    let caption = document.createElement("p");
+    
+    // Set the caption to the alt text of the image.
+    caption.innerHTML = image.getAttribute("alt");
+
+    newDiv.appendChild(caption);
+}
+
+// First image on the page.
+let testImage = images[0];
+
+
+
+
+
