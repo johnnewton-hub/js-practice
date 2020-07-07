9f599798c1dda8707ffbee2e40a540e8d5ee5c0c
James Grieve <jgrieve@ualberta.ca>
2020-07-07 15:24:17 -0600

    JavaScript almost done;

--- Shopping List/js/scripts.js
+++ Shopping List/js/scripts.js
@@ -8,18 +8,46 @@
     const addButton = document.querySelector("a");
     // <ul class="empty">
     const list = document.querySelector("ul");
+    // Running list of all our items.
+    let itemArray = [];
 
     addButton.addEventListener("click", () =>
         {
-            // 1. Create a list item element.
-            // 2. Add the text from the text field to the list item.
-            // 3. Add the list item to the list.
-                // 3a. If it is the first item, remove the empty class from
-                // the list.
-                // 3b. If it is the first item, remove the "No items yet!" item.
-            // 4. Clear the text input. 
+            // TODO: Add Error Output to User
 
+            // Prevent Empty List Items (Including Whitespace) and Prevent Duplicate List Items
+            if (input.value.trim() !== "" && !itemArray.includes(input.value.trim()))
+            {
+                // 1. Create a list item element.
+                let newListItem = document.createElement("li");
 
+                // 2. Add the text from the text field to the list item.
+                // P.S. Also removing trailing and leading whitespace.
+                newListItem.innerText = input.value.trim();
+
+                itemArray.push(input.value.trim());
+
+                if (list.className === "empty")
+                {
+                    // 3a. If it is the first item, remove the empty class from
+                    // the list.
+                    list.classList.remove("empty");
+                
+                    // 3b. If it is the first item, remove the "No items yet!" item.
+                    // If the list still has the empty class, it should only
+                    // have the "no items" li.
+                    list.children[0].remove();
+
+                }
+                // 3. Add the list item to the list.
+                list.appendChild(newListItem);
+                    
+                // 4. Clear the text input. 
+                input.value = "";
+
+                // Reset Focus To Input After Adding
+                input.focus();
+            }
         }
     );
     
