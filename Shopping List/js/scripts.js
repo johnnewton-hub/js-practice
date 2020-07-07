becac2c2a5bbc893c8c0a737af6e25c10e545edd
James Grieve <jgrieve@ualberta.ca>
2020-07-07 14:59:30 -0600

    Step 1 - create list item;

--- Shopping List/js/scripts.js
+++ Shopping List/js/scripts.js
@@ -11,7 +11,15 @@
 
     addButton.addEventListener("click", () =>
         {
-            console.log("Stuff has been done.");
+            // 1. Create a list item element.
+            // 2. Add the text from the text field to the list item.
+            // 3. Add the list item to the list.
+                // 3a. If it is the first item, remove the empty class from
+                // the list.
+                // 3b. If it is the first item, remove the "No items yet!" item.
+            // 4. Clear the text input. 
+
+
         }
     );
     
