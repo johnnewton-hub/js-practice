82cace07db066a992d4644417b5276eefdfeb9ae
James Grieve <jgrieve@ualberta.ca>
2020-07-09 11:43:04 -0600

    Added parents and children text example;

--- JS Only/js/scripts.js
+++ JS Only/js/scripts.js
@@ -2,10 +2,39 @@
 const body = document.querySelector("body");
 
 // Create the first element we need, the container div that is in the center of the page.
-let containerDiv = document.createElement("div");
+const containerDiv = document.createElement("div");
 
 // Add the container class to the div. Typically best to do element modifications before pushing into the DOM.
 containerDiv.classList.add("container");
 
 // Append the new div to body.
 body.appendChild(containerDiv);
+/*
+<body>
+    <div class="container">
+    </div>
+</body>
+*/
+
+
+// Append a header and a main to the container.
+containerDiv.appendChild(document.createElement("header"));
+/*
+<body>
+    <div class="container">
+        <header>
+        </header>
+    </div>
+</body>
+*/
+containerDiv.appendChild(document.createElement("main"));
+/*
+<body>
+    <div class="container">
+        <header>
+        </header>
+        <main>
+        </main>
+    </div>
+</body>
+*/
